7a04ce5f7d5d4a638527994ffbcb2a4ddee8c8df
Sheikh Zubair <sheikhzubair998@gmail.com>
2019-03-17 18:45:09 +0500

    py shell debug

--- predictor.js
+++ predictor.js
@@ -3,14 +3,16 @@ const { PythonShell } = require('python-shell');
 
 module.exports = (img_data) => {
     console.log('predictor called')
+    let py = (process.env.NODE_ENV === 'production')? process.env.PYTHONPATH : '/usr/bin/python3'
     const options = {
         mode: 'text',
         //encodeing: 'utf8',
-        pythonPath: '/usr/bin/python3',
+        pythonPath: py,
         pythonOptions: ['-u'], // get print results in real-time
         scriptPath: './ml_model',
         args: [JSON.stringify(img_data)]
     };
+    console.log(py)
     console.log('creating promise')
     let pyshell = new PythonShell('data_wrangle.py', options)
     return new Promise((resolve, reject) => {
